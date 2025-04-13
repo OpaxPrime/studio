e1734bcb6fd88f63970310c9e188ab03cc7f98cf
Orion Cole <opaxprime020@gmail.com>
2025-04-13 18:59:46 +0000

    add in that the instagram: the instagram page name is tubeimproveai

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -20,7 +20,7 @@ export default function ContactPage() {
       </p>
       <ul className="list-none pl-0 mb-4">
         <li>
-          <a href="https://instagram.com/tubeimproveai" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://instagram.com/tubeimproveai" target="_blank" rel="noopener noreferrer">Instagram: @tubeimproveai</a>
         </li>
       </ul>
       <p className="text-md mb-4">
